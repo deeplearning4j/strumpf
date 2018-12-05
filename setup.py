1516ba73a67aec8f2ca2f5f6667d464fb42efe93
Max Pumperla <max.pumperla@googlemail.com>
2018-12-05 16:36:56 +0100

    v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='strumpf',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=['azure', 'requests', 'click', 'argcomplete'],
     extras_require={
