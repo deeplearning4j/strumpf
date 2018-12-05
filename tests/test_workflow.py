935d4ab80518dafa3a041d435d0419cbef255645
Max Pumperla <max.pumperla@googlemail.com>
2018-12-05 16:36:37 +0100

    test workflow

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -83,12 +83,13 @@ def test_workflow():
     assert os.path.isfile(large_a_path + REF)
     assert os.path.isfile(large_b_path + REF)
 
-    # Connect to azure to check remote files
+    # Connect to azure to check for remote files
     azure = strumpf.service_from_config()
     blobs = azure.get_all_blob_names()
 
     # Confirm that zipped files and references are available on azure
-    assert large_a + ZIP in blobs
+    # The file comes as version "v1", reference files get updated.
+    assert large_a + ZIP + '.v1' in blobs
     assert large_a + REF in blobs
 
     # Confirm that original (not zipped) files and references have been cached as well
@@ -98,22 +99,42 @@ def test_workflow():
     assert os.path.isfile(os.path.join(cache, large_a + REF))
 
     # Try to download file. Since it's in cache AND hashes equal, no need to download again
-    was_downloaded = cli.download(large_a)
+    was_downloaded, _ = cli.download(large_a)
     assert not was_downloaded
 
     # Now, clear cache and attempt download again. This time it will download and unzip the file
     strumpf._clear_cache()
     assert not os.path.isfile(os.path.join(cache, large_a))
 
-    was_downloaded = cli.download(large_a)
+    # Version 1 gets downloaded (we validate internally that it's the right hash as well)
+    was_downloaded, version = cli.download(large_a)
     assert was_downloaded
+    assert version == 1
     assert os.path.isfile(os.path.join(cache, large_a))
+
+    # Finally, create an updated version of "large_a" and upload it again.
+    _create_large_test_file(os.path.join(local_dir, large_a_path), 2 * limit)
+    cli.add(large_a_path)
+    with mock.patch('strumpf.core.input', return_value='y'):
+        cli.upload()
+
+    # Now we have two versioned, remote files and one reference
+    blobs = azure.get_all_blob_names()
+    assert large_a + ZIP + '.v1' in blobs
+    assert large_a + ZIP + '.v2' in blobs
+    assert large_a + REF in blobs
+
+    # This time version 2 gets downloaded
+    strumpf._clear_cache()
+    was_downloaded, version = cli.download(large_a)
+    assert was_downloaded
+    assert version == 2
 
     # Cleaning up
     os.remove(large_a_path + REF)
     os.remove(large_b_path + REF)
     os.remove(small_path)
-
+    strumpf._clear_cache()
 
 if __name__ == '__main__':
     pytest.main([__file__])
