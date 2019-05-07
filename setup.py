743ac562dc284c53c9275f001c6a9a386fef3dc9
Max Pumperla <max.pumperla@googlemail.com>
2019-05-07 16:48:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='strumpf',
-    version='0.2.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=['azure', 'requests', 'click', 'argcomplete'],
     extras_require={
